2753b287db450707ac7b0864ae646dca9c703963
William Kondo <willkondo@gmail.com>
2022-03-16 09:07:51 -0300

    Include list invested stocks event in render process

--- src/preload/configure_preload.ts
+++ src/preload/configure_preload.ts
@@ -4,6 +4,8 @@ import { contextBridge, ipcRenderer } from 'electron';
 const configurePreload = () => {
   contextBridge.exposeInMainWorld('stocks', {
     load: (type: StockType) => ipcRenderer.invoke('stocks:load', type),
+    listInvested: (type: StockType) =>
+      ipcRenderer.invoke('stocks:listInvested', type),
   });
 };
 
