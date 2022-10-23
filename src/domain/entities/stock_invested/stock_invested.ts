943ecda5aa1525accb95768ed18c187215ceb24a
William Kondo <willkondo@gmail.com>
2022-10-23 02:00:10 -0300

    Fix imports

--- src/domain/entities/stock_invested/stock_invested.ts
+++ src/domain/entities/stock_invested/stock_invested.ts
@@ -1,5 +1,5 @@
-import Price from '../price/price';
-import Stock from '../stock/stock';
+import Price from '@entities/price/price';
+import Stock from '@entities/stock/stock';
 
 type StockInvested = {
   stock: Stock;
