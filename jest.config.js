c83981401cfbc6c3585c98d4db25a93915127ad6
William Kondo <willkondo@gmail.com>
2022-10-23 20:34:29 -0300

    Create @usecases

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,12 @@ module.exports = {
   maxWorkers: 1,
   moduleNameMapper: {
     '^@components(.*)$': '<rootDir>/src/renderer/components$1',
+    '^@entities(.*)$': '<rootDir>/src/domain/entities$1', 
     '^@errors(.*)$': '<rootDir>/src/domain/errors$1',
+    '^@repositories(.*)$': '<rootDir>/src/domain/repositories$1',
     '^@services(.*)$': '<rootDir>/src/main/services$1',
     '^@sqlite(.*)$': '<rootDir>/src/main/services/sqlite$1',
+    '^@usecases(.*)$': '<rootDir>/src/domain/usecases$1',
   },
   setupFilesAfterEnv: ['<rootDir>/src/tests/setup.ts'],
 };
